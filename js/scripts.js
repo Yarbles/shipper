064b760cfdc6d68ce1ca79330e6ae475ab278d77
Kelly de Vries & Reid Baldwin & bountham <student@epicodus.com>
2015-04-14 15:44:01 -0700

    fix display of final cost and fix algorithm for shipping cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
 $(document).ready(function() {
-    s=$('form#shipper').submit(function(event) {
+    $('form#shipper').submit(function(event) {
         event.preventDefault();
 
         var inputted_distance = parseInt($("input#distance").val());
         var inputted_weight = parseInt($("input#weight").val());
-        var inputted_speed = parseInt($("input#speed").val());
+        var inputted_speed = parseInt($("select#speed").val());
         var cost = { distance: inputted_distance,
                      weight: inputted_weight,
                      speed: inputted_speed,
                      shipper: function() {
-                         0.05*this.distance + 2*this.weight + -1.5*this.speed;
+                         return ((0.05*this.distance + 2*this.weight) * this.speed).toFixed(2);
+
                      }};
+
+
+
+                $("#result").text(cost.shipper());
+                $("#result-shipper").show();
+
+
+                    $("input#distance").val("");
+                    $("input#weight").val("");
+                    $("input#speed").val("");
+
+
     });
 });
